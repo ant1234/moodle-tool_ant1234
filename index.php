ac61e43f11f2951034a53c8a5cb5ee4a64493895
Antony Videtta <antonyvidetta@catalyst.net.nz>
2021-01-13 10:50:22 +1300

    Add simple input and output

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 
 require_once(__DIR__ . '/../../../config.php');
+
+
 $url = new moodle_url('/admin/tool/ant1234/index.php');
 $PAGE->set_context(context_system::instance());
 $PAGE->set_url($url);
@@ -8,5 +10,13 @@ $PAGE->set_pagelayout('report');
 $PAGE->set_title('Hello to the todo list');
 $PAGE->set_heading(get_string('pluginname', 'tool_ant1234'));
 
-echo get_string('display', 'tool_ant1234');
-// echo get_string('healthnoproblemsfound', 'tool_ant1234');
+$output = $OUTPUT;
+
+echo $output->header();
+
+$greeting = get_string('display', 'tool_ant1234');
+
+echo html_writer::div(format_text($greeting), '', array('id' => 'course')); // Used for multil-line rich-text contents such as forum post body.
+
+echo $output->footer();
+
