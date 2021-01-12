726af807d43e5f5fc484be4d2bb975328e0ee01c
Antony Videtta <antonyvidetta@catalyst.net.nz>
2021-01-12 14:39:52 +1300

    Initialise moodle plugin

---
+++ version.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Version details.
+ *
+ * @package    tool
+ * @copyright  2021 Ant
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$plugin->version   = 2021011200; // The current plugin version (Date: YYYYMMDDXX)
+$plugin->requires  = 2021052500; // Requires this Moodle version
+$plugin->component = 'tool_ant1234'; // Full name of the plugin (used for diagnostics)
+$plugin->release = '1.0';
